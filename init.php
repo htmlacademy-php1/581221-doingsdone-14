d69f9095ee6a54a54c0bbedd1a4e0ea52bb7deb5
artemevskih <artemevskih@gmail.com>
2022-05-16 13:15:26 +0300

    Выводим данные из базы

---
+++ init.php
@@ -0,0 +1,12 @@
+<?php
+
+    require_once 'functions.php';
+    $db = require_once 'db.php';
+
+    $link = mysqli_connect($db['host'], $db['user'], $db['password'], $db['database']);
+    mysqli_set_charset($link, 'charset: "utf8"');
+
+    $categories = [];
+    $content = '';
+
+?>
